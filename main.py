fdce51ba84b8d4444500fd63ac78d4796e067fa0
mitalidaduria <94062583+mitalidaduria@users.noreply.github.com>
2022-10-20 01:42:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("leap year.")
+else:
+    print("Not a leap year.")            
